de45d1c7b43adb1fbfd894c878f7cce88351f3dc
alphanumerichyphen <47741315+alphanumerichyphen@users.noreply.github.com>
2021-02-17 22:59:05 +0530

    comment user/admin, import render_template, python in HTML
    Passing data to html using render_template(), using passed data with {{}} in html,  writing python in html using {%%}

--- something.py
+++ something.py
@@ -1,19 +1,19 @@
-from flask import Flask, redirect, url_for
+from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def homepage():
-	return "Konichiwa! Tanjiro-san"
-
 @app.route("/<name>")
+def homepage(name):
+	return render_template("index.html", content=name)
+
+'''@app.route("/<name>")
 def user(name):
 	return f"Hello {name}!"
 
 
 @app.route("/admin")
 def admin():
-	return redirect(url_for("homepage"))
-
+	return redirect(url_for("user, name=Admin"))
+'''
 if __name__ == "__main__":
-	app.run()
+	app.run(debug = True)
