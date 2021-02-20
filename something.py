ef454edcf58ee491bcc75d7551463ee69e1e49a5
alphanumerichyphen <47741315+alphanumerichyphen@users.noreply.github.com>
2021-02-20 21:08:18 +0530

    template inheritance, adding bootstrap navbar
    using  {% block "whatever" %}{% endblock %}, and adding bootstrap elementsin head and body

--- something.py
+++ something.py
@@ -2,18 +2,23 @@ from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
-@app.route("/<name>")
-def homepage(name):
-	return render_template("index.html", content=name)
+@app.route("/")
+def homepage():
+	return render_template("index.html")
 
-'''@app.route("/<name>")
+@app.route("/<name>")
 def user(name):
-	return f"Hello {name}!"
+	return render_template("user.html", content=name)
 
 
 @app.route("/admin")
 def admin():
-	return redirect(url_for("user, name=Admin"))
-'''
+	return redirect(url_for("user", name="Admin"))
+
+
+@app.route("/test")
+def test(name="test"):
+	return f"<h1>If life is a {name}, take and A+ to the graveyard</h1>"
+
 if __name__ == "__main__":
 	app.run(debug = True)
